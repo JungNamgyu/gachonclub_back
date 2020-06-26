56c333fae2031ba953ea8a6a79e3b779e40a0ba7
JungNamgyu <jng6017@naver.com>
2020-06-26 21:28:11 +0900

    로컬데이터컨버터

---
+++ src/main/java/kr/ac/gachon/gc/gachonclub_back/Configuration/LocalDateAttributeConverter.java
@@ -0,0 +1,20 @@
+package kr.ac.gachon.gc.gachonclub_back.Configuration;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Converter(autoApply = true)
+public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(LocalDate locDate) {
+        return locDate == null ? null : Date.valueOf(locDate);
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date sqlDate) {
+        return sqlDate == null ? null : sqlDate.toLocalDate();
+    }
+}
